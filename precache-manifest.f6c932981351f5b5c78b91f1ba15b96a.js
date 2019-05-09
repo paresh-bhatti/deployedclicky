7ce99ba03bc1b709acba7948285105bf08788630
Par-B <pareshbhatti@bellsouth.net>
2019-05-09 13:51:29 -0400

    Updates

---
+++ precache-manifest.f6c932981351f5b5c78b91f1ba15b96a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45738abf7eea909f7652fc169edebc01",
+    "url": "/deployedclicky/index.html"
+  },
+  {
+    "revision": "12ad39f3faba3ab3923f",
+    "url": "/deployedclicky/static/css/main.2482646f.chunk.css"
+  },
+  {
+    "revision": "0154f9689bcca81074f4",
+    "url": "/deployedclicky/static/js/2.be2527d6.chunk.js"
+  },
+  {
+    "revision": "12ad39f3faba3ab3923f",
+    "url": "/deployedclicky/static/js/main.0f54959f.chunk.js"
+  },
+  {
+    "revision": "d727bb44f84ac82a5b45",
+    "url": "/deployedclicky/static/js/runtime~main.d1e50f32.js"
+  },
+  {
+    "revision": "93f56132c6881c3fb25c58d8770194fc",
+    "url": "/deployedclicky/static/media/Aquarius.93f56132.jpg"
+  },
+  {
+    "revision": "d9294ddb0562e3bd05c93f8d1cff5a83",
+    "url": "/deployedclicky/static/media/Aries.d9294ddb.jpg"
+  },
+  {
+    "revision": "ef9bf48f0888066a4d3c252325053d33",
+    "url": "/deployedclicky/static/media/Cancer.ef9bf48f.jpg"
+  },
+  {
+    "revision": "6dddeb51f4e6c6ef9404837dba8c1e24",
+    "url": "/deployedclicky/static/media/Capricorn.6dddeb51.jpg"
+  },
+  {
+    "revision": "882195015b4f47902cae5127eb9fd238",
+    "url": "/deployedclicky/static/media/Gemini.88219501.jpg"
+  },
+  {
+    "revision": "e5ee959f46f07c7856f5bf94728b5b4c",
+    "url": "/deployedclicky/static/media/Leo.e5ee959f.jpg"
+  },
+  {
+    "revision": "e7b6915f531681d604d1bf5330237f16",
+    "url": "/deployedclicky/static/media/Pisces.e7b6915f.jpg"
+  },
+  {
+    "revision": "8fd860d508a68b8d7c1e769d3ba66197",
+    "url": "/deployedclicky/static/media/Sagittarius.8fd860d5.jpg"
+  },
+  {
+    "revision": "85f8a67d71c415168b1b8903e37686fd",
+    "url": "/deployedclicky/static/media/Scorpio.85f8a67d.jpg"
+  },
+  {
+    "revision": "6dddeb51f4e6c6ef9404837dba8c1e24",
+    "url": "/deployedclicky/static/media/Taurus.6dddeb51.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployedclicky/static/media/logo.5d5d9eef.svg"
+  }
+]);
